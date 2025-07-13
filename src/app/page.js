1ceb407c2331575be5653a84ead0bfc7a09a2827
Nimra <NimraMughalCode>
2025-07-13 18:44:40 +0500

    i am tired

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
     // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
  
     // </div>
-    <div className="gap-4 font-sans">
+    <div className="gap-4 bg-black font-sans">
     <HeroCarousel /> 
       <CategoryList />
       <WhyChooseUs />
